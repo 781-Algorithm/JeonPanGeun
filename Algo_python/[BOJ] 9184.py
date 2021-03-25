dcb00bcb052e6d30beb5648b069129630c0ef2c8
Jason9789 <lifebook0809@gmail.com>
2021-03-25 14:25:20 +0900

    [SOL] BOJ 9184 신나는 함수 실행

--- Algo_python/[BOJ] 9184.py
+++ Algo_python/[BOJ] 9184.py
@@ -1,18 +1,32 @@
-import sys
+from sys import stdin
+
+dp = [[[0] * 100 for _ in range(100)] for _ in range(100)]
 
 def w(a, b, c):
+
+    if dp[a][b][c]:
+        return dp[a][b][c]
+
     if a <= 0 or b <= 0 or c <= 0:
         return 1
+
     elif a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
-    elif a < b and b < c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+
+    elif a < b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+
     else:
-        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return dp[a][b][c]
 
 
-x, y, z = map(int, sys.stdin.readline().split())
+while True:
+    x, y, z = map(int, stdin.readline().split())
 
-result = w(x, y, z)
+    if x == -1 and y == -1 and z == -1:
+        break
 
-print("w(%d, %d, %d) = %d" % (x, y, z, result))
+    result = w(x, y, z)
+    print("w(%d, %d, %d) = %d" % (x, y, z, result))
