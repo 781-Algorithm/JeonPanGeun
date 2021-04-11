9ae58cd8e9cc712baab8cf0f52ed11737cff18ee
Jason9789 <lifebook0809@gmail.com>
2021-04-11 15:51:46 +0900

    sol: [BOJ] 7568

---
+++ Algo_python/[BOJ] 7568.py
@@ -0,0 +1,16 @@
+
+N = int(input())
+students = []
+
+for _ in range(N):
+    weight, height = map(int, input().split())
+    students.append((weight, height))
+
+
+for i in students:
+    rank = 1
+    for j in students:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+
+    print(rank, end = " ")
